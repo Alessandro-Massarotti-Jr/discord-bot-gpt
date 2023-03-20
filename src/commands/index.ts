714d36425eed71b3ba6159cde9bb60521f8a445f
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-03-19 21:33:58 -0300

    refactor(General): Remove white spaces

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,10 +2,6 @@ import { chatGptCommand } from "./chat-gpt";
 import { ICommandInterface } from "./ICommandInterface";
 import { pingCommand } from "./ping";
 
-
-
-
-
 const commands: ICommandInterface[] = [pingCommand, chatGptCommand];
 
 export { commands }
